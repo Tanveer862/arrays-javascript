e2fe19cc27374debbfd030e9d26eaef1ec7b1bb8
Tanveer862 <tanveer.hussain0227@gmail.com>
2024-10-02 20:06:58 +0500

    append two classes

---
+++ DocumentObject/8_practice.js
@@ -0,0 +1,6 @@
+let para=document.querySelector("p")
+para.getAttribute("class")
+//para.setAttribute("class","newClass")// add new class exist class cannot work on it 
+para.classList.add("newClass")
+
+
