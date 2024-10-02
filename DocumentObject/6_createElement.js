a41db94f2e02bfd3b1e40923154b51ebaf3ed42d
Tanveer862 <tanveer.hussain0227@gmail.com>
2024-10-02 18:38:34 +0500

    add button

--- DocumentObject/6_createElement.js
+++ DocumentObject/6_createElement.js
@@ -2,3 +2,6 @@ let newBtn=document.createElement("button")
 newBtn.innerText="Click me!"
 let div=document.querySelector("div")
 div.append(newBtn)
+let heading=document.createElement("h1")
+heading.innerHTML="Hy i am new "
+document.querySelector("body").prepend(heading)
