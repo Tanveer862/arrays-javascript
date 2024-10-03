199fc89b3d4f895ad44c53a3b41315469fd36425
Tanveer862 <tanveer.hussain0227@gmail.com>
2024-10-03 13:51:05 +0500

    change style

--- DocumentObject/6_createElement.js
+++ DocumentObject/6_createElement.js
@@ -1,7 +1,17 @@
 let newBtn=document.createElement("button")
 newBtn.innerText="Click me!"
+newBtn.style.backgroundColor="blue"
+newBtn.style.color="white"
 let div=document.querySelector("div")
 div.append(newBtn)
 let heading=document.createElement("h1")
 heading.innerHTML="Hy i am new "
 document.querySelector("body").prepend(heading)
+heading.style.color="blue"
+heading.style.font="bold"
+let list=document.querySelectorAll("li")
+console.log(list)
+list[0].style.color="green"
+list[1].style.color="green"
+list[2].style.color="green"
+let input=document.createElement("input")
