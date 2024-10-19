26f5a5296b7ed90ee754e78b3e66c25aeee24273
Tanveer862 <tanveer.hussain0227@gmail.com>
2024-10-19 17:59:26 +0500

    add delete button

--- eventProject/script.js
+++ eventProject/script.js
@@ -1,5 +1,4 @@
-let myUser='';
-
+// let myUser=null;
 let box1=document.getElementById("box1")
 let box2=document.getElementById("box2")
 let box3=document.getElementById("box3")
@@ -9,15 +8,60 @@ let addUser=document.getElementById("add")
 let deleteUser=document.getElementById("del")
 let moveYellow =document.getElementById("move")
 let moveGreen=document.getElementById("green")
-let amoveBrown=document.getElementById("brown")
+let moveBrown=document.getElementById("brown")
 let moveChocolate=document.getElementById("chocolate")
 
 function inputValue(){
-  let myUser=document.createElement('div')
-  myUser.setAttribute('id',"myId");
-  myUser.className="myClass";
-  myUser.innerHTML=input.value
-  myUser.style.color="black"
-  box1.append(myUser);
+ let myUser;
+  if(myUser==null){
+    let  myUser=document.createElement('div')
+    myUser.setAttribute('id',"myId");
+    myUser.className="myClass";
+    myUser.innerHTML=input.value
+    myUser.style.color="black"
+    input.disabled=true;
+    addUser.disabled=true;
+    deleteUser.disabled=false;
+    box1.append(myUser);
+
+  }
 }
+   function deleteUserFromProject(){
+  let myUser=document.getElementById("myId")
+  if(myUser !=null){
+    myUser.remove();
+    input.disabled=false;
+    addUser.disabled=false;
+    deleteUser.disabled=true;
+    input.value="";
+    myUser=null;
+  }
+}
+ function moveToYellow(){
+  alert("yellow")
+  box1.append(myUser)
+
+ }
+ function moveToGreen(){
+  alert("green")
+  box2.append(myUser)
+ }
+ function moveToBrown(){
+  alert("brown")
+  box3.append(myUser)
+  
+ }
+ function moveToChocolate(){
+  alert("chocolate")
+  box4.append(myUser)
+  
+ }
+
+
+
 addUser.addEventListener("click",inputValue)
+deleteUser.addEventListener("click",deleteUserFromProject)
+moveYellow.addEventListener("click",moveToYellow)
+moveGreen.addEventListener("click",moveToGreen)
+moveBrown.addEventListener("click",moveToBrown)
+moveChocolate.addEventListener("click",moveToChocolate)
