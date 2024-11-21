9d099474ec1c874071d4cf5f02bc44ebdaf181c9
Tanveer862 <tanveer.hussain0227@gmail.com>
2024-11-21 19:50:19 +0500

    maximimum hight in array and number of times repeat hight

--- This week/question16.js
+++ This week/question16.js
@@ -1,3 +1,6 @@
+// arr[1,3,5,7,9]
+//The minimum sum is 1+3+5+7=16  and the maximum sum is 3+5+7+9=24 . The function prints
+
 function miniMaxSum(arr) {
     let temp;
     let maxSum=0;
@@ -15,13 +18,33 @@ function miniMaxSum(arr) {
     }
 }
 console.log(arr)
-for(let k=1;k<arr.length;k++){
-    maxSum=maxSum+arr[k]
+for(let k=0;k<arr.length;k++){
+    if(k!=0){
+        maxSum=maxSum+arr[k];
+    }
+    if(k!=arr.length-1){
+        minSum=minSum+arr[k]
+    }
 }
-
-for(let j=0;j<arr.length-1;j++){
-    minSum=minSum+arr[j]
-}
+// for(let k=0;k<arr.length;k++){
+//     if(k==0){
+//         minSum=minSum+arr[k]
+//     } else if(k==arr.length-1){
+//         maxSum=maxSum+arr[k];
+//     }
+//     else{
+//         maxSum=maxSum+arr[k];
+//         minSum=minSum+arr[k];
+//     } 
+// }
 console.log(minSum,maxSum);
+// for(let k=1;k<arr.length;k++){
+//     maxSum=maxSum+arr[k]
+// }
+
+// for(let j=0;j<arr.length-1;j++){
+//     minSum=minSum+arr[j]
+// }
+// console.log(minSum,maxSum);
 }
 miniMaxSum([1,2,3,4,5])
---
+++ This week/question17.js
@@ -0,0 +1,19 @@
+// candles=[4,4,1,3]
+///The maximum height candles are 4 units high. There are 2 of them, so return 2 .
+function birthdayCakeCandles(candles) {
+    let maxBirth=candles[0];
+    let count=0;
+    for(let i=1;i<candles.length;i++){
+            if(candles[i]>maxBirth){
+                maxBirth=candles[i]
+            }
+    }
+    for(let k=0;k<candles.length;k++){
+if(candles[k]===maxBirth){
+    count++;
+}
+    }
+    console.log(maxBirth)
+console.log(count)
+}
+birthdayCakeCandles([3,4,5,6,9,9,6,7,])
